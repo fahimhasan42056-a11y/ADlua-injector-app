fa4b6259e7fb4601a05d1a278a85bcee4fb9aca4
fahimhasan42056-a11y <fahimhasan42056@gmail.com>
2025-09-03 12:07:33 +0600

    Update MainActivity.java

--- app/src/main/java/com/example/adluainjector/MainActivity.java
+++ app/src/main/java/com/example/adluainjector/MainActivity.java
@@ -1,1 +1,30 @@
+package com.example.adluainjector;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // আমাদের বোতাম খুঁজে আনা
+        Button injectButton = findViewById(R.id.injectButton);
+
+        // বোতামে ক্লিক করলে কী হবে সেটা লেখা
+        injectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // এখানে তুমি আসল কাজ বসাতে পারো
+                Toast.makeText(MainActivity.this,
+                        "🚀 Injection Started!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
